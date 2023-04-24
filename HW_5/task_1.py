ebe98ab3a32c5c06622c12b0789598bc69458a7c
ToniaRomaniuk <roxyromanik@gmail.com>
2023-04-24 11:38:19 +0300

    fixed task #2 from HW #5

--- HW_5/task_1.py
+++ HW_5/task_1.py
@@ -6,9 +6,10 @@
 a = int(input('Введите число: '))
 b = int(input('Введите степень числа: '))
 def degree(a, b):
-    if (b == 1):
-        return (a)
-    if (a != 1):
-        return (a * degree(a, b - 1))
+    if b == 1:
+        return a
+    if a != 1:
+        return a * degree(a, b - 1)
+        
   
 print('a =',a, 'b =',b, '->', degree(a, b))        
